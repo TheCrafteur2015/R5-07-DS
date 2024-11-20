e81cace5772ff28a6cb468813e97b3bdc61874c2
Atlonos <58007069+TheCrafteur2015@users.noreply.github.com>
2024-11-20 14:03:44 +0100

    Update Q3.java

--- Q3.java
+++ Q3.java
@@ -1,7 +1,7 @@
 public class Q3 {
 
 	public static void main(String[] args) {
-		if (args.length >= 1) {
+		if (args.length == 1) {
 			int count = 0;
 			try {
 				count = Integer.parseInt(args[0]);
