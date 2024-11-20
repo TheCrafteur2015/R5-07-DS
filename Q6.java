4ad905bec98422335223dfd38a961c4a24cdd7c4
Atlonos <58007069+TheCrafteur2015@users.noreply.github.com>
2024-11-20 14:38:35 +0100

    Create Q6.java

---
+++ Q6.java
@@ -0,0 +1,18 @@
+public class Q6 {
+
+	public static void main(String[] args) {
+		if (args.length == 1) {
+			int count = 0;
+			try {
+				count = Integer.parseInt(args[0]);
+			} catch (NumberFormatException e) {
+				System.out.println("Sorry but it is not a number...");
+				System.exit();
+			}
+			for (int i = count; i > 0; i--)
+        		System.out.println("x".repeat(i));
+		} else {
+			throw new IllegalArgumentException();
+		}
+	}
+}
