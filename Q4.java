93484ea64f8cd0ccc7fc5d9feece6ac2e110ea6a
Atlonos <58007069+TheCrafteur2015@users.noreply.github.com>
2024-11-20 14:25:53 +0100

    Create Q4.java

---
+++ Q4.java
@@ -0,0 +1,18 @@
+public class Q4 {
+
+	public static void main(String[] args) {
+		if (args.length == 1) {
+			int count = 0;
+			try {
+				count = Integer.parseInt(args[0]);
+			} catch (NumberFormatException e) {
+				System.out.println("Sorry but it is not a number...");
+				System.exit();
+			}
+			for (int i = 0; i < count; i++)
+        		System.out.println("*".repeat(i + 1));
+		} else {
+			throw new IllegalArgumentException();
+		}
+	}
+}
